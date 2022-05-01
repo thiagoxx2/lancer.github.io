5b5fe7f2439fe597622b13c2a5f35b9946ce7ab5
thiago.xavier <thiagosousaxavier@hotmail.com>
2022-05-01 00:31:39 -0300

    Primeiro Commit

---
+++ functions.js
@@ -0,0 +1,13 @@
+var my_element = document.getElementById("description");
+
+my_element.scrollIntoView({
+  behavior: "smooth",
+  block: "start",
+  inline: "nearest"
+});
+
+
+function botaoAciona() {
+    alert("Teste Disso");
+    console.log("funcionoyu");
+}
